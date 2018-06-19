f9679bbe683c204487d7badfbbba39d50ed32626
773974336@qq.com <773974336@qq.com>
2018-06-19 15:33:08 +0800

    complete graphsage

--- graphsage/parcellation_aggregators.py
+++ graphsage/parcellation_aggregators.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-
-import random
+from torch.nn import init
 
 """
 Set of modules for aggregating embeddings of neighbors.
@@ -12,7 +11,7 @@ class MeanAggregator(nn.Module):
     """
     Aggregates a node's embeddings using mean of neighbors' embeddings
     """
-    def __init__(self, features, cuda=False): 
+    def __init__(self, features, embed_dim): 
         """
         Initializes the aggregator for a specific graph.
 
@@ -24,9 +23,11 @@ class MeanAggregator(nn.Module):
         super(MeanAggregator, self).__init__()
 
         self.features = features
-        self.cuda = cuda
+        self.embed_dim = embed_dim
+        self.weight = nn.Parameter(torch.FloatTensor(embed_dim, 2 * self.feat_dim))
+        init.xavier_uniform(self.weight)
 
-    def forward(self, raw_featuers, nodes, to_neighs):
+    def forward(self, raw_features, nodes, to_neighs):
         """
         nodes --- list of nodes in a batch
         like  [491, 988, 1446, 1611, 2217, 824, 494, 799, 1001, 847]
@@ -46,26 +47,19 @@ class MeanAggregator(nn.Module):
         num_sample --- number of neighbors to sample. No sampling if None.
         """
         samp_neighs = to_neighs
-        samp_neighs = [samp_neigh + set([nodes[i]]) for i, samp_neigh in enumerate(samp_neighs)]
         unique_nodes_list = list(set.union(*samp_neighs))
         unique_nodes = {n:i for i,n in enumerate(unique_nodes_list)}
         mask = Variable(torch.zeros(len(samp_neighs), len(unique_nodes)))
-        column_indices = [unique_nodes[n] for samp_neigh in samp_neighs for n in samp_neigh]   
+        column_indices = [unique_nodes[n] for samp_neigh in samp_neighs for n in samp_neigh]
         row_indices = [i for i in range(len(samp_neighs)) for j in range(len(samp_neighs[i]))]
         mask[row_indices, column_indices] = 1
-        if self.cuda:
-            mask = mask.cuda()
+        mask = mask.cuda()
         num_neigh = mask.sum(1, keepdim=True)
         mask = mask.div(num_neigh)
         if self.features:
-            if self.cuda:
-                embed_matrix = self.features(raw_featuers, torch.LongTensor(unique_nodes_list).cuda())
-            else:
-                embed_matrix = self.features(raw_featuers, torch.LongTensor(unique_nodes_list))
+            embed_matrix = self.features(raw_features, torch.LongTensor(unique_nodes_list))
         else:
-            if self.cuda:
-                embed_matrix = raw_featuers(torch.LongTensor(unique_nodes_list).cuda())
-            else:
-                embed_matrix = raw_featuers(torch.LongTensor(unique_nodes_list))
+            embed_matrix = raw_features(torch.LongTensor(unique_nodes_list)).cuda()
+            
         to_feats = mask.mm(embed_matrix)
         return to_feats
--- graphsage/parcellation_encoders.py
+++ graphsage/parcellation_encoders.py
@@ -8,8 +8,7 @@ class Encoder(nn.Module):
     Encodes a node's using 'convolutional' GraphSage approach
     """
     def __init__(self, features, feature_dim, 
-            embed_dim, adj_lists, aggregator,
-            base_model=None, gcn=False, cuda=False, 
+            embed_dim, adj_lists, aggregator, 
             feature_transform=False): 
         super(Encoder, self).__init__()
 
@@ -17,33 +16,27 @@ class Encoder(nn.Module):
         self.feat_dim = feature_dim
         self.adj_lists = adj_lists
         self.aggregator = aggregator
-        if base_model != None:
-            self.base_model = base_model
 
-        self.gcn = gcn
         self.embed_dim = embed_dim
-        self.cuda = cuda
-        self.aggregator.cuda = cuda
         self.weight = nn.Parameter(
-                torch.FloatTensor(embed_dim, self.feat_dim if self.gcn else 2 * self.feat_dim))
+                torch.FloatTensor(embed_dim, 2 * self.feat_dim))
         init.xavier_uniform(self.weight)
+        self.relu = nn.LeakyReLU()
 
-    def forward(self, nodes):
+    def forward(self, raw_features, nodes):
         """
         Generates embeddings for a batch of nodes.
 
         nodes     -- list of nodes
         """
-        if self.features:
-            neigh_feats = self.aggregator.forward(nodes, [self.adj_lists[node] for node in nodes])
-       
-        if not self.gcn:
-            if self.cuda:
-                self_feats = self.features(torch.LongTensor(nodes).cuda())
-            else:
-                self_feats = self.features(torch.LongTensor(nodes))
-            combined = torch.cat([self_feats, neigh_feats], dim=1)
+
+        neigh_feats = self.aggregator.forward(raw_features, nodes, [self.adj_lists[node] for node in nodes])
+
+        if self.features:      
+            self_feats = self.features(raw_features, torch.LongTensor(nodes))
         else:
-            combined = neigh_feats
-        combined = F.relu(self.weight.mm(combined.t()))
+            self_feats = raw_features(torch.LongTensor(nodes)).cuda()
+                
+        combined = torch.cat([self_feats, neigh_feats], dim=1)
+        combined = self.relu(self.weight.mm(combined.t()))
         return combined
